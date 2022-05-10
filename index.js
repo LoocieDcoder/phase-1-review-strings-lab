b3467bddd9d024845f9a4424f2444be1b8c110a2
Lucy <quaysonlucyofficial@gmail.com>
2022-05-10 09:22:31 +0100

    Assignment completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 const currentUser = 'Lucy Quayson';
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ${currentUser.slice(0, 1)}!';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
 
